62272255a129b7f9db8b2405c1d5779945f1e5a3
Ashwinkarthik Selvaraj <b6rd31sv@students.codeinstitute.net>
2024-08-11 18:10:20 +0000

    Create Wire up read_insights page to display grod with insights from MOngoDB

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ from flask import (
     redirect, request, session, url_for)
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
+from datetime import datetime
 from werkzeug.security import generate_password_hash, check_password_hash
 if os.path.exists("env.py"):
     import env
@@ -202,7 +203,11 @@ def delete_site(locations_id):
 def read_insights():
     reviews = mongo.db.reviews.find()
     return render_template("read_insights.html", reviews=reviews)
-    return render_template("read_insights.html")
+    
+
+
+
+
 
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
